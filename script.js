4334fcdb76d723219072abf0b82c8734b4b641b4
vaisy <40660896+vaisy@users.noreply.github.com>
2020-02-14 09:50:43 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,71 @@
-var carousel = document.querySelector('.carousel');
-if (carousel != null)
-    carousel.setAttribute('style', 'display:block;');
-var container = carousel.querySelector('.carousel-container');
-var prevBtn = carousel.querySelector('.carousel-prev');
-var nextBtn = carousel.querySelector('.carousel-next');
-var pagination = carousel.querySelector('.carousel-pagination');
-var bullets = [].slice.call(carousel.querySelectorAll('.carousel-bullet'));
-var totalItems = container.querySelectorAll('.carousel-item').length;
-var percent = (100 / totalItems);
-var currentIndex = 0;
+//var carousel = document.querySelector('.carousel');
+//if (carousel != null)
+//    carousel.setAttribute('style', 'display:block;');
+//var container = carousel.querySelector('.carousel-container');
+//var prevBtn = carousel.querySelector('.carousel-prev');
+//var nextBtn = carousel.querySelector('.carousel-next');
+//var pagination = carousel.querySelector('.carousel-pagination');
+//var bullets = [].slice.call(carousel.querySelectorAll('.carousel-bullet'));
+//var totalItems = container.querySelectorAll('.carousel-item').length;
+//var percent = (100 / totalItems);
+//var currentIndex = 0;
 
-function next() {
-    slideTo(currentIndex + 1);
-}
+//function next() {
+//    slideTo(currentIndex + 1);
+//}
 
-function prev() {
-    slideTo(currentIndex - 1);
-}
+//function prev() {
+//    slideTo(currentIndex - 1);
+//}
 
-function slideTo(index) {
-    index = index < 0 ? totalItems - 1 : index >= totalItems ? 0 : index;
-    container.style.WebkitTransform = container.style.transform = 'translate(-' + (index * percent) + '%, 0)';
-    bullets[currentIndex].classList.remove('active-bullet');
-    bullets[index].classList.add('active-bullet');
-    currentIndex = index;
-}
+//function slideTo(index) {
+//    index = index < 0 ? totalItems - 1 : index >= totalItems ? 0 : index;
+//    container.style.WebkitTransform = container.style.transform = 'translate(-' + (index * percent) + '%, 0)';
+//    bullets[currentIndex].classList.remove('active-bullet');
+//    bullets[index].classList.add('active-bullet');
+//    currentIndex = index;
+//}
 
-bullets[currentIndex].classList.add('active-bullet');
-prevBtn.addEventListener('click', prev, false);
-nextBtn.addEventListener('click', next, false);
+//bullets[currentIndex].classList.add('active-bullet');
+//prevBtn.addEventListener('click', prev, false);
+//nextBtn.addEventListener('click', next, false);
 
-pagination.addEventListener('click', function (e) {
-    var index = bullets.indexOf(e.target);
-    if (index !== -1 && index !== currentIndex) {
-        slideTo(index);
+//pagination.addEventListener('click', function (e) {
+//    var index = bullets.indexOf(e.target);
+//    if (index !== -1 && index !== currentIndex) {
+//        slideTo(index);
+//    }
+//}, false);
+
+$(document).ready(function () {
+
+    var carouselContainer = $('.carousel');
+    var slideInterval = 5000000;
+
+    function toggleH() {
+        $('.toggleCaption').hide()
+        var caption = carouselContainer.find('.active').find('.toggleCaption').addClass('animated fadeInRight').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+
+            function () {
+                $(this).removeClass('animated fadeInRight')
+            });
+        caption.slideToggle();
     }
-}, false);
+
+    function toggleC() {
+        $('.toggleButton').hide()
+        var button = carouselContainer.find('.active').find('.toggleButton').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+
+            function () {
+                $(this).removeClass('animated fadeInUp')
+            });
+        button.slideToggle();
+    }
+    carouselContainer.carousel({
+        interval: slideInterval,
+        cycle: true,
+        pause: "hover"
+    })
+        .on('slide.bs.carousel slid.bs.carousel', toggleH).trigger('slide.bs.carousel')
+        .on('slide.bs.carousel slid.bs.carousel', toggleC).trigger('slide.bs.carousel');
+});
